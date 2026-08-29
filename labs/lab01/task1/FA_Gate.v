a86bb7d1ec599c646184367391cde7deb6ac4cd7
Parth Srivastava <parthsrivastava801@gmail.com>
2026-08-29 17:37:52 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,11 +16,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  and #(2) (pc1, a,   b);
-  xor #(2) (sum, cin, ps);
-  or  #(2) (cout, pc1, pc2);
-  and #(2) (pc2, cin, ps);
-  xor #(2) (ps,  a,   b);
+  and (pc1, a,   b);
+  xor (sum, cin, ps);
+  or  (cout, pc1, pc2);
+  and (pc2, cin, ps);
+  xor (ps,  a,   b);
 
 // Task 1(b):
 // Reordering the gate instantiations does not change the waveform or output.
